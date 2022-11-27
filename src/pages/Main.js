3841e7c7c2260da04c57111122db139aefd3c956
euichan2 <euichan1108@gmail.com>
2022-11-27 23:16:59 +0900

    schedule dnd

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,7 @@ import Search from "components/Search";
 import TagGroup from "components/TagGroup";
 import OrderDropDown from "components/OrderDropDown";
 import FilterDropDown from "components/FilterDropDown";
-import {useRecoilValue, useSetRecoilState} from "recoil";
+import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {itemsState, tagsState} from "states";
 
 import styles from "assets/Main.module.css";
@@ -11,10 +11,11 @@ import CardGrid from "components/CardGrid";
 import MyScheduler from 'components/MyScheduler';
 import {closestCenter, DndContext, KeyboardSensor, PointerSensor, useSensor, useSensors} from "@dnd-kit/core";
 import {arrayMove, sortableKeyboardCoordinates} from "@dnd-kit/sortable";
+import {StatusType} from "../types";
 
 const Main = () => {
     const [activeId, setActiveId] = useState(null);
-    const setItems = useSetRecoilState(itemsState);
+    const [items, setItems] = useRecoilState(itemsState);
     const tags = useRecoilValue(tagsState);
 
     useEffect(() => {
@@ -36,13 +37,31 @@ const Main = () => {
         const { active, over } = event;
 
         if (active !== over) {
-            setItems((items) => {
-                console.log(active, over);
-                const oldIndex = items.findIndex((item) => item.id === active.id);
-                const newIndex = items.findIndex((item) => item.id === over.id);
+            console.log(active, over)
+            if(over.id === 'schedule'){
+                setItems(prev => {
+                    return prev.map(item => {
+                        if(active.id === item.id){
+                            console.log("here");
+                            return {
+                                ...item,
+                                status: StatusType.DRAGGED
+                            }
+                        } else {
+                            return item;
+                        }
+                    })
+                })
+            } else {
+                setItems((items) => {
+                    const oldIndex = items.findIndex((item) => item.id === active.id);
+                    const newIndex = items.findIndex((item) => item.id === over.id);
 
-                return arrayMove(items, oldIndex, newIndex);
-            });
+                    return arrayMove(items, oldIndex, newIndex);
+                });
+            }
+            console.log("items", items);
+
         }
     }
 
