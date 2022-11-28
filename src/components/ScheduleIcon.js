c82671b4d2803fc9a45cc2f4c0b46179fa130571
PRESSO <caff1nepill@gmail.com>
2022-11-29 01:46:06 +0900

    change scheduleicon style

--- src/components/ScheduleIcon.js
+++ src/components/ScheduleIcon.js
@@ -1,6 +1,8 @@
 import {IoCalendarOutline} from "react-icons/io5";
 import {useDroppable} from "@dnd-kit/core";
 import styles from "assets/Main.module.css";
+import {SmoothCorners, SmoothCornersWrapper} from "react-smooth-corners";
+import React from "react";
 
 const ScheduleIcon = ({id}) => {
     const {isOver, setNodeRef} = useDroppable({
@@ -8,13 +10,34 @@ const ScheduleIcon = ({id}) => {
     });
 
     const style = {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         zIndex: isOver ? "100" : "auto",
-        opacity: isOver ? 0.5 : 1
+        opacity: isOver ? 0.5 : 1,
     };
+
     return (
-        <div className={styles.iconContainer} ref={setNodeRef} style={style}>
-            <IoCalendarOutline className={styles.icon}/>
-        </div>
+        // <div className={styles.iconContainer} ref={setNodeRef} style={style}>
+        //     <IoCalendarOutline className={styles.icon} />
+        // </div>
+
+        <SmoothCornersWrapper
+            shadow="4px 0 10px rgb(0, 0, 0, 0.4)"
+            style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: 999 }}
+        >
+            <SmoothCorners
+                corners="3"
+                borderRadius="12px"
+                className={styles.iconContainer}
+            >
+                <div style={style} ref={setNodeRef}>
+                    <IoCalendarOutline className={styles.icon} />
+                </div>
+            </SmoothCorners>
+        </SmoothCornersWrapper>
     )
 }
 
