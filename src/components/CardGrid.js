5678a15d76f1d7f66291a584565b2a2d85a5b35b
Hyeon-hak Kim <96914905+ooMia@users.noreply.github.com>
2022-11-29 08:57:14 +0900

    기능 철회
    추가된 일정을 투명하게 만드는 스타일 삭제

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {
-     rectSortingStrategy,
+    rectSortingStrategy,
     SortableContext,
 } from '@dnd-kit/sortable';
-import {SortableItem} from 'components/SortableItem';
+import { SortableItem } from 'components/SortableItem';
 
 import styles from "assets/CardGrid.module.css";
-import {itemsState} from "states";
-import {useRecoilValue} from "recoil";
-import {StatusType} from "../types";
+import { itemsState } from "states";
+import { useRecoilValue } from "recoil";
+import { StatusType } from "../types";
+
+import { FixedSizeGrid } from "react-window";
 
 const CardGrid = () => {
 
@@ -17,29 +19,58 @@ const CardGrid = () => {
 
     return (
 
-            <div className={styles.container}>
-                <SortableContext
-                    items={items}
-                    strategy={rectSortingStrategy}
-                >
-                    {
-                        items.map((item) => {
-                            if(item.status === StatusType.SHOWEN) {
-                                return (
-                                    <SortableItem
-                                        key={`item-${item.id}`}
-                                        id={item.id}
-                                        item={item}
-                                    />
-                                )
-                            } else {
-                                return null;
-                            }
-                        })
-                    }
-                </SortableContext>
-            </div>
+        <div className={styles.container}>
+            <SortableContext
+                items={items}
+                strategy={rectSortingStrategy}
+            >
+                
+                {
+                    items.map((item) => {
+                        if (item.status === StatusType.SHOWEN) {
+                            return (
+                                <SortableItem
+                                    key={`item-${item.id}`}
+                                    id={item.id}
+                                    item={item}
+                                />
+                            )
+                        } else {
+                            return null;
+                        }
+                    })
+                }
+            </SortableContext>
+        </div>
 
+    );
+}
+
+const CardTable = () => {
+    const dataList = useRecoilValue(itemsState);
+
+    const Cell = ({ columnIndex, rowIndex, style }) => (
+        <div style={{ ...style, ...{ display: "flex", whiteSpace: "pre-wrap" } }}>
+            <img
+                src={dataList[rowIndex * 4 + columnIndex].avatar}
+                alt={dataList[rowIndex * 4 + columnIndex].name}
+                width={50}
+            />
+            <p>{dataList[rowIndex * 4 + columnIndex].name} - {dataList[rowIndex * 4 + columnIndex].email}</p>
+        </div>
+    );
+
+    return (
+        <FixedSizeGrid
+            height={(window.innerHeight - 20)}
+            width={(window.innerWidth - 20)}
+            columnCount={4}
+            rowCount={dataList.length}
+            columnWidth={(window.innerHeight) / 2}
+            rowHeight={80}
+        >
+            {Cell}
+        </FixedSizeGrid >
     );
 }
 
