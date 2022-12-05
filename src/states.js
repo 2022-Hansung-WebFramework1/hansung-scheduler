b414d173203207949c4cfd0d9c83a9ee822f8fc4
euichan2 <euichan1108@gmail.com>
2022-12-06 01:08:18 +0900

    save local storage

--- src/states.js
+++ src/states.js
@@ -1,11 +1,30 @@
 import { atom } from "recoil";
 import data from "data.json";
 import {StatusType} from "types";
+import {reactLocalStorage} from 'reactjs-localstorage';
 
-const dataHandling = () => {
-    const _data = JSON.parse(JSON.stringify(data)).data;
-    return _data.map(data => {return {...data, status: StatusType.SHOWEN}});
+const dataHandling = (key) => {
+    const savedValue = reactLocalStorage.get(key);
+    if (savedValue !== undefined) {
+        return savedValue;
+    } else {
+        const _data = JSON.parse(JSON.stringify(data)).data;
+        return _data.map(data => {return {...data, status: StatusType.SHOWEN}});
+    }
 }
+
+const localStorageEffect = key => ({setSelf, onSet}) => {
+    const savedValue = reactLocalStorage.get(key)
+    if (savedValue !== undefined) {
+        setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+        isReset
+            ? reactLocalStorage.remove(key)
+            : reactLocalStorage.set(key, JSON.stringify(newValue));
+    });
+};
 
 export const tagsState = atom({
     key: 'tagsState',
@@ -14,5 +33,8 @@ export const tagsState = atom({
 
 export const itemsState = atom({
     key: 'itemsState',
-    default: dataHandling()
+    default: dataHandling('schedules'),
+    effects: [
+        localStorageEffect('schedules'),
+    ]
 });
